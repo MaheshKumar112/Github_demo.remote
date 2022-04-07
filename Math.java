910a1ed428f8ff4880e7a812a84236e9aa4d33ee
MaheshKumar112 <103049887+MaheshKumar112@users.noreply.github.com>
2022-04-07 13:07:09 +0530

    Add files via upload

---
+++ Math.java
@@ -0,0 +1,33 @@
+public static void main(String [] args)
+{
+    int a=20;
+    int b=30;
+    int c= a+b;
+    System.out.println(c);
+}
+//Substraction implementation
+public int sub(int d,int e)
+{
+    return d-e;
+}
+//Multiplication implementation
+public int mul()
+{
+    return a*b ;
+}
+//System.out.println("JAI NTR");
+//Division implementation
+public int div()
+{
+    return a/b;
+}
+<<<<<<< HEAD
+//Implementation of MODULUS FUNCTION
+=======
+System.out.println("JAI BALAYYA");
+>>>>>>> 0ccfb70 (Printed about the young and dynamic hero in NTR branch)
+public int mod()
+    {
+        reurn a%b;
+
+    }
